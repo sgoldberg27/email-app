1841cf15f8969bfeb5833f8a1d78d8d64526ea9a
sofigoldberg <sofigoldberg@gmail.com>
2024-01-24 23:31:28 -0300

    Messages get list

--- db.ts
+++ db.ts
@@ -7,10 +7,59 @@ type Folder = {
     icon: string;
 };
 
+type Message = {
+    id: number;
+    from: {
+        name: string;
+        avatar: string;
+        email: string;
+    };
+    to: {
+        name: string;
+        email: string;
+    }[];
+    subject: string;
+    message: string;
+    time: string;
+    read: boolean;
+    important: boolean;
+    hasAttachments: boolean;
+    attachments: {
+        type: string;
+        fileName: string;
+        preview: string;
+        url: string;
+        size: string;
+    }[];
+    labels: string[];
+};
+
+
 export async function getFolders() {
     const foldersFile = await fs.readFile("./db/folders.json", "utf-8");
     const foldersJson = JSON.parse(foldersFile);
 
     return foldersJson["data"] as Folder[];
+
+}
+
+export async function getMessages(from_name: any, to_name: any, subject: any) {
+    const messagesFile = await fs.readFile("./db/important.json", "utf-8");
+    const messagesJson = JSON.parse(messagesFile);
+
+    const messages = messagesJson["data"] as Message[];
+
+    const filteredMessages = messages.filter((message) => {
+        const hasFromName = message.from.name.includes(from_name);
+        const hasToName = message.to.some((to) => to.name.includes(to_name));
+        const hasSubject = message.subject.includes(subject);
+
+        console.log(hasFromName, hasToName, hasSubject);
+
+        return hasFromName && hasToName && hasSubject;
+    });
+
+    return filteredMessages;
+
 
 }
--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
 import express from "express"
-import { getFolders } from "./db";
+import { getFolders, getMessages } from "./db";
 
 const app = express()
 const port = 3000
 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
+
+// Get all folders
 app.get("/:usuario/api/folders", async (req, res) => {
     res.send(await getFolders());
+});
+
+
+// Get the list of messages
+app.get("/:usuario/api/messages/important", async (req, res) => {
+
+    let from = req.query.from;
+    let to = req.query.to;
+    let subject = req.query.subject;
+
+    res.send(await getMessages(from, to, subject));
 });
