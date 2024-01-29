6ce0eb7c33a275c723e0bee36e67f90445162359
sofigoldberg <sofigoldberg@gmail.com>
2024-01-29 12:08:18 -0300

    delete or add message to starred or important folders if needed

--- db.ts
+++ db.ts
@@ -29,6 +29,7 @@ const MessageSchema = z.object({
     message: z.string(),
     time: z.string(),
     read: z.boolean(),
+    starred: z.boolean(),
     important: z.boolean(),
     hasAttachments: z.boolean(),
     labels: z.array(z.string()),
@@ -56,6 +57,27 @@ async function writeFile(filePath: string, data: any) {
     await fs.writeFile(filePath, JSON.stringify(data));
 }
 
+// Function to send a message to a specific folder
+async function sendToFolder(folder: string, message: Message) {
+    const folderJson = await readFile(`./db/${folder}.json`);
+
+    const folderMessages = folderJson["data"] as Message[];
+    folderMessages.push(message);
+
+    folderJson["data"] = folderMessages;
+    await writeFile(`./db/${folder}.json`, folderJson);
+}
+
+// Function to delete a message from a folder
+async function deleteFromFolder(folder: string, id: string){
+    const folderJson = await readFile(`./db/${folder}.json`);
+    const folderMessages = folderJson["data"] as Message[];
+    const filteredMessages = folderMessages.filter((message) => message.id !== id);
+    folderJson["data"] = filteredMessages;
+    await writeFile(`./db/${folder}.json`, folderJson);
+}
+
+
 // Function to get all folders
 export async function getFolders() {
     const foldersJson = await readFile("./db/folders.json");
@@ -64,10 +86,10 @@ export async function getFolders() {
 
 // Function to get the list of messages that match the search criteria
 export async function getMessages(folder: string, from_name: string, to_name: string, subject: string) {
-    const messagesJson = await readFile(`./db/${folder}.json`);
-    const messages = messagesJson["data"] as Message[];
+    const folderJson = await readFile(`./db/${folder}.json`);
+    const folderMessages = folderJson["data"] as Message[];
 
-    const filteredMessages = messages.filter((message) => {
+    const filteredMessages = folderMessages.filter((message) => {
         const hasFromName = message.from.name.includes(from_name);
         const hasToName = message.to.some((to) => to.name.includes(to_name));
         const hasSubject = message.subject.includes(subject);
@@ -78,31 +100,29 @@ export async function getMessages(folder: string, from_name: string, to_name: st
     return filteredMessages;
 }
 
-// Function to send a message to the trash folder
-async function sendToTrash(message: Message) {
-    const trashJson = await readFile("./db/trash.json");
-
-    const trashMessages = trashJson["data"] as Message[];
-    trashMessages.push(message);
-
-    trashJson["data"] = trashMessages;
-    await writeFile("./db/trash.json", trashJson);
-}
 
 // Function to delete a message
 export async function deleteMessages(folder: string, id: string) {
-    const messagesJson = await readFile(`./db/${folder}.json`);
+    const folderJson = await readFile(`./db/${folder}.json`);
 
-    const messages = messagesJson["data"] as Message[];
+    const folderMessages = folderJson["data"] as Message[];
 
-    const deletedMessage = messages.find((message) => message.id === id);
-    const filteredMessages = messages.filter((message) => message.id !== id);
-
-    messagesJson["data"] = filteredMessages;
-    await writeFile(`./db/${folder}.json`, messagesJson);
+    const deletedMessage = folderMessages.find((message) => message.id === id);
+    const filteredMessages = folderMessages.filter((message) => message.id !== id);
 
     if (deletedMessage) {
-        await sendToTrash(deletedMessage);
+        folderJson["data"] = filteredMessages;
+        await writeFile(`./db/${folder}.json`, folderJson);
+
+        if (deletedMessage.important && folder !== "important") {
+            await deleteFromFolder("important", deletedMessage.id);
+        }
+
+        if (deletedMessage.starred && folder !== "starred") {
+            await deleteFromFolder("starred", deletedMessage.id);
+        }
+
+        await sendToFolder("trash", deletedMessage);
         return "OK";
     } else {
         throw new CustomError("No se encontro el mensaje", 404);
@@ -114,17 +134,19 @@ export async function createMessages(folder: string, newMessage: Message) {
     newMessage["id"] = uuidv4();
     const validationResult = MessageSchema.safeParse(newMessage);
 
-    if(validationResult.success) {
-        const importantJson = await readFile(`./db/${folder}.json`);
+    if (validationResult.success) {
+        await sendToFolder(folder, newMessage);
 
-        const importantMessages = importantJson["data"] as Message[];
+        if (newMessage.important && folder !== "important") {
+            await sendToFolder("important", newMessage);
+        }
 
-        importantMessages.push(newMessage);
-
-        importantJson["data"] = importantMessages;
-        await writeFile(`./db/${folder}.json`, importantJson);
+        if (newMessage.starred && folder !== "starred") {
+            await sendToFolder("starred", newMessage);
+        }
 
         return newMessage;
+
     } else {
         throw new CustomError("Invalid message body", 400);
     }
