643d5008a24e54163e55739e93d38a96f94954a8
sofigoldberg <sofigoldberg@gmail.com>
2024-01-29 19:58:16 -0300

    error handling refactor

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express from "express"
 import { getFolders, getMessages, deleteMessages, createMessages, CustomError } from "./db";
 import type { Message } from "./db";
 import multer from 'multer';
-
+import { Request, Response, NextFunction } from "express";
 
 const app = express()
 const port = 3000
@@ -24,72 +24,54 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
 
+const handleErrors = (handler: (req: Request, res: Response, next: NextFunction) => Promise<any>) => async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        await handler(req, res, next);
+    } catch (error) {
+        if (error instanceof CustomError) {
+            res.status(error.status).send(error.message);
+        } else {
+            res.status(500).send("Internal server error");
+        }
+    }
+}
 
 // Get all folders
-app.get("/:usuario/api/folders", async (req, res) => {
-    try {
-        res.send(await getFolders());
-    } catch (error) {
-        console.error("Error getting folders", error);
-        res.status(500).send("Internal server error");
-    }
-});
+app.get("/:usuario/api/folders", handleErrors(async (req, res) => {
+    res.send(await getFolders());
+}));
 
 
 // Get the list of messages
-app.get("/:usuario/api/messages/:folder", async (req, res) => {
-    try {
-        let from = req.query.from as string;
-        let to = req.query.to as string;
-        let subject = req.query.subject as string;
-        
-        let folder = req.params.folder as string;
+app.get("/:usuario/api/messages/:folder", handleErrors(async (req, res) => {
+    const from = req.query.from as string;
+    const to = req.query.to as string;
+    const subject = req.query.subject as string;
+    const folder = req.params.folder as string;
 
-        res.send(await getMessages(folder, from, to, subject));
-    } catch (error) {
-        console.error("Error creating message", error);
-        if (error instanceof CustomError) {
-            res.status(error.status).send(error.message);
-        } else {
-            res.status(500).send("Internal server error");
-        }
-    }
-});
+    res.send(await getMessages(folder, from, to, subject));
+}));
 
 // Delete messages
-app.delete("/:usuario/api/messages/:folder/:id", async (req, res) => {
-    try {
-        let id = req.params.id as string;
-        let folder = req.params.folder as string;
-        res.send(await deleteMessages(folder, id));
-    } catch (error) {
-        console.error("Error deleting message", error);
-        if (error instanceof CustomError) {
-            res.status(error.status).send(error.message);
-        } else {
-            res.status(500).send("Internal server error");
-        }
-    }
-});
+app.delete("/:usuario/api/messages/:folder/:id", handleErrors(async (req, res) => {
+    const id = req.params.id as string;
+    const folder = req.params.folder as string;
+
+    res.send(await deleteMessages(folder, id));
+}));
 
 app.use(express.json());
 
 // Create messages
-app.post("/:usuario/api/messages/:folder", upload.array('attatchments'), async (req, res) => {
-    try {
-        let message = req.body as Message;
-        let folder = req.params.folder as string;
+app.post("/:usuario/api/messages/:folder", upload.array('attatchments'), handleErrors(async (req, res) => {
+    const message = req.body as Message;
+    const folder = req.params.folder as string;
 
-        const attatchments = req.files as Express.Multer.File[];
+    const attatchments = req.files as Express.Multer.File[];
 
-        res.send(await createMessages(folder, message));
-    } catch (error) {
-        console.error("Error creating message", error);
-        if (error instanceof CustomError) {
-            res.status(error.status).send(error.message);
-        } else {
-            res.status(500).send("Internal server error");
-        }
-
-    }
-});
+    res.send(await createMessages(folder, message));
+}));
